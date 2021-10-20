20f18cfe33a1cbe3ceed04676d4de74fbfbaae30
Eli Mei <meimison84@gmail.com>
2021-10-20 16:30:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace zzxx_docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
